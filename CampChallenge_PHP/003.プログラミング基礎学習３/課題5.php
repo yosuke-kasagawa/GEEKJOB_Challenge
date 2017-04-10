346e10d0978c5ff620eca10833c8a4644d9bc4cf
笠川陽介 <yosuke.kasagawa@gmail.com>
2017-04-10 15:22:44 +0900

    PHP-プログラミング基礎3の課題5を提出します

---
+++ CampChallenge_PHP/003.プログラミング基礎学習３/課題5.php
@@ -0,0 +1,20 @@
+<?php
+
+  function registration ($id, $name, $birthday, $address) {
+    $info = array(
+      'id' => $id,
+      'name' => $name,
+      'birthday' => $birthday,
+      'address' => $address
+    );
+    return $info;
+  }
+
+  $members = array();
+  $members[] = registration(111,'kasagawa','1990/10/28','162-0825');
+  $members[] = registration(112,'sato','1995/1/1','162-0001');
+  $members[] = registration(113,'tanaka','1985/12/31','162-9999');
+
+  echo "<PRE>";
+  print_r($members);
+  echo "<PRE>";
