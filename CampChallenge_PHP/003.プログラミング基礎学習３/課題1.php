a87beaaf202062ca4f8f9e15367b8f319106256c
笠川陽介 <yosuke.kasagawa@gmail.com>
2017-04-10 15:21:06 +0900

    PHP-プログラミング基礎3の課題1を提出します

---
+++ CampChallenge_PHP/003.プログラミング基礎学習３/課題1.php
@@ -0,0 +1,11 @@
+<?php
+
+  function profile() {
+    echo '笠川陽介です。<br>';
+    echo '1990年10月28日生まれです。<br>';
+    echo '富士そばと小諸そばが好きです。<br><br>';
+  }
+
+  for ($count = 0; $count < 10; $count ++) {
+    profile();
+  }
