36c2b1ed0846bd8bb8bc84bb8af26526a93877d0
笠川陽介 <yosuke.kasagawa@gmail.com>
2017-05-02 14:28:19 +0900

    PHP-PHPからのDB操作の課題1を提出します

---
+++ CampChallenge_PHP/007.PHPからのDB操作/task1.php
@@ -0,0 +1,19 @@
+<?php
+
+  define('DB_DATABASE', 'challenge_db');
+  define('DB_USERNAME', 'kasagawa');
+  define('DB_PASSWORD', 'kasagawa');
+  define('PDO_DSN', 'mysql:host=localhost;dbname=' . DB_DATABASE . ';charset=utf8');
+
+  try {
+
+    $pdo_object = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
+    $pdo_object->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo '接続しました！';
+
+    $pdo_object＝null;
+
+  } catch (PDOException $Exception) {
+    die('接続に失敗しました: '.$Exception);
+  }
